14f45cbf777b707dcc69b7d94bdc4998c402fceb
Ruuuuuuub <ruben.isdahl@proton.me>
2025-04-11 22:43:49 +0200

    Fix

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // Links
 const HOME = "/";
-const LOGIN = "/login.html";
-const REGISTER = "/register.html";
-const FORGOT_PASSWORD = "/forgotPassword.html";
-const MAINTENANCE = "/133aw/maint_sqd/home.html";
-const MAINT = "/133aw/maint_sqd/maint.html";
-const QA = "/133aw/maint_sqd/qa.html";
-const BU = "/133aw/maint_sqd/bu.html";
-const RU = "/133aw/maint_sqd/ru.html";
-const MOC = "/133aw/maint_sqd/moc.html";
-const CAMO = "/133aw/maint_sqd/camo.html";
-const LVT = "/133aw/maint_sqd/lvt.html";
-const LEADERSHIP = "/133aw/maint_sqd/leadership.html";
-const COMPETENCE = "/133aw/maint_sqd/competence-planner.html";
+const LOGIN = "login.html";
+const REGISTER = "register.html";
+const FORGOT_PASSWORD = "forgotPassword.html";
+const MAINTENANCE = "133aw/maint_sqd/home.html";
+const MAINT = "133aw/maint_sqd/maint.html";
+const QA = "133aw/maint_sqd/qa.html";
+const BU = "133aw/maint_sqd/bu.html";
+const RU = "133aw/maint_sqd/ru.html";
+const MOC = "133aw/maint_sqd/moc.html";
+const CAMO = "133aw/maint_sqd/camo.html";
+const LVT = "133aw/maint_sqd/lvt.html";
+const LEADERSHIP = "133aw/maint_sqd/leadership.html";
+const COMPETENCE = "133aw/maint_sqd/competence-planner.html";
 
 // Text content
 const FOOTER = "© 2025 Ruben Isdahl";
