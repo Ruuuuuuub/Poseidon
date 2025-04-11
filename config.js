1ed7efbe85fd881c1f2d22861c3fec8165211a2c
Ruuuuuuub <ruben.isdahl@proton.me>
2025-04-12 00:45:42 +0200

    Maybe

--- config.js
+++ config.js
@@ -3,16 +3,18 @@ const HOME = "";
 const LOGIN = "login.html";
 const REGISTER = "register.html";
 const FORGOT_PASSWORD = "forgotPassword.html";
+
+// 133AW Maintenance Squadron
 const MAINTENANCE = "133aw/maint_sqd/home.html";
-const MAINT = "../../maint.html";
-const QA = "../../qa.html";
-const BU = "../../bu.html";
-const RU = "../../ru.html";
-const MOC = "../../moc.html";
-const CAMO = "../../camo.html";
-const LVT = "../../lvt.html";
-const LEADERSHIP = "../../leadership.html";
-const COMPETENCE = "../../competence-planner.html";
+const MAINT = "/133aw/maint_sqd/maint.html";
+const QA = "/133aw/maint_sqd/qa.html";
+const BU = "/133aw/maint_sqd/bu.html";
+const RU = "/133aw/maint_sqd/ru.html";
+const MOC = "/133aw/maint_sqd/moc.html";
+const CAMO = "/133aw/maint_sqd/camo.html";
+const LVT = "/133aw/maint_sqd/lvt.html";
+const LEADERSHIP = "/133aw/maint_sqd/leadership.html";
+const COMPETENCE = "/133aw/maint_sqd/competence-planner.html";
 
 // Text content
 const FOOTER = "© 2025 Ruben Isdahl";
